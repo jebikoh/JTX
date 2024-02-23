8da565253d8cba9b7954fa32930c2b9d0331903f
Jayden E <aedara@uw.edu>
2024-02-23 11:40:32 -0800

    Add basic rendering init/free tests

--- test/renderer_test.c
+++ test/renderer_test.c
@@ -3,16 +3,43 @@
 #include <check.h>
 #include <stdlib.h>
 
-// START_TEST(test_renderer_init) {
-//   Renderer r;
-//   int w = 100;
-//   int h = 50;
-//   Camera c =
-// }
+START_TEST(test_renderer_init) {
+  Renderer r;
+  int w = 100;
+  int h = 50;
+  Camera c = camera_new(vec3_new(0.0f, 0.0f, 30.0f), vec3_new(0.0f, 0.0f, 0.0f),
+                        vec3_new(0.0f, 1.0f, 0.0f), 1.0472f, 0.1f, 100.0f);
+  renderer_init(&r, w, h, NULL, c);
+  ck_assert_int_eq(r.w, w);
+  ck_assert_int_eq(r.h, h);
+  ck_assert_float_eq(r.ar, 2.0f);
+  ck_assert_int_eq(r.mlen, 111);
+  ck_assert_float_eq(r.c.fov, 1.0472f);
+  ck_assert_ptr_nonnull(r.zbuf);
+  ck_assert_ptr_nonnull(r.fb);
+  ck_assert_float_eq(r.zbuf[0], -INFINITY);
 
-// START_TEST(test_renderer_init_extfb) {
+  renderer_free(&r, 0);
+}
 
-// }
+START_TEST(test_renderer_init_extfb) {
+  Renderer r;
+  int w = 100;
+  int h = 50;
+  Camera c = camera_new(vec3_new(0.0f, 0.0f, 30.0f), vec3_new(0.0f, 0.0f, 0.0f),
+                        vec3_new(0.0f, 1.0f, 0.0f), 1.0472f, 0.1f, 100.0f);
+  float *fb = calloc(w * h, sizeof(float));
+  renderer_init(&r, w, h, &fb, c);
+  ck_assert_int_eq(r.w, w);
+  ck_assert_int_eq(r.h, h);
+  ck_assert_float_eq(r.ar, 2.0f);
+  ck_assert_int_eq(r.mlen, 111);
+  ck_assert_float_eq(r.c.fov, 1.0472f);
+  ck_assert_ptr_nonnull(r.zbuf);
+  ck_assert_ptr_nonnull(r.fb);
+  ck_assert_float_eq(r.zbuf[0], -INFINITY);
+  renderer_free(&r, 1);
+}
 
 Suite *renderer_suite(void) {
   Suite *s;
@@ -22,7 +49,8 @@ Suite *renderer_suite(void) {
 
   tc_core = tcase_create("Core");
 
-  // tcase_add_test(tc_core, test_dirlight_init);
+  tcase_add_test(tc_core, test_renderer_init);
+  tcase_add_test(tc_core, test_renderer_init_extfb);
   suite_add_tcase(s, tc_core);
 
   return s;

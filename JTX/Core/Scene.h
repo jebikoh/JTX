d50dcd11a2fd553bac5b8efdb3929ec15a0a36fa
Jayden E <aedara@uw.edu>
2024-04-09 00:15:25 -0700

    Add pass-by-reference to scenes for now

--- JTX/Core/Scene.h
+++ JTX/Core/Scene.h
@@ -6,21 +6,65 @@
 #include "JTX/Util/Util.h"
 
 namespace JTX::Core {
-        class Scene {
-        public:
-            using PrimitiveID = uint64_t;
-            using LightID = uint64_t;
+    class Scene {
+    public:
+        using PrimitiveID = uint64_t;
+        using LightID = uint64_t;
 
-            explicit Scene(JTX::Core::Camera camera) : camera(camera) {};
-            ~Scene() = default;
+        explicit Scene(JTX::Core::Camera camera) : camera(camera) {}
+        ~Scene() = default;
 
-        private:
-            std::unordered_map<PrimitiveID, JTX::Core::Primitive*> primitives;
-            std::unordered_map<LightID, JTX::Core::DirLight*> lights;
-            JTX::Core::Camera camera;
 
-            JTX::Util::IDPool primitiveIDPool;
-            JTX::Util::IDPool lightIDPool;
-        };
+        PrimitiveID addPrimitive(JTX::Core::Primitive &primitive) {
+            PrimitiveID id = primitiveIDPool.getID();
+            primitives[id] = &primitive;
+            return id;
+        }
 
+        void removePrimitive(PrimitiveID id) {
+            if (primitives.find(id) != primitives.end()) {
+                primitives.erase(id);
+                primitiveIDPool.releaseID(id);
+            }
+        }
+
+        Primitive &getPrimitive(PrimitiveID id) {
+            if (primitives.find(id) != primitives.end()) {
+                return *primitives[id];
+            }
+            throw std::runtime_error("Primitive with ID " + std::to_string(id) + " not found");
+        }
+
+        LightID addLight(DirLight &light) {
+            LightID id = lightIDPool.getID();
+            lights[id] = &light;
+            return id;
+        }
+
+        void removeLight(LightID id) {
+            if (lights.find(id) != lights.end()) {
+                lights.erase(id);
+                lightIDPool.releaseID(id);
+            }
+        }
+
+        DirLight &getLight(LightID id) {
+            if (lights.find(id) != lights.end()) {
+                return *lights[id];
+            }
+            throw std::runtime_error("Light with ID " + std::to_string(id) + " not found");
+        }
+
+        JTX::Core::Camera &getCamera() { return camera; }
+
+
+    private:
+        JTX::Core::Camera camera;
+
+        std::unordered_map<PrimitiveID, JTX::Core::Primitive*> primitives;
+        std::unordered_map<LightID, JTX::Core::DirLight*> lights;
+
+        JTX::Util::IDPool primitiveIDPool;
+        JTX::Util::IDPool lightIDPool;
+    };
 }

93a4e8ca8fc458ec83cfb9730e6ed99e8edda62a
Jayden E <aedara@uw.edu>
2024-04-10 16:50:59 -0700

    Finish drawLine(); update drawPoint() to inline; Fix SEGFAULT bug

--- JTX/Core/Renderer.cpp
+++ JTX/Core/Renderer.cpp
@@ -7,22 +7,77 @@ JTX::Core::Renderer::Renderer(int w, int h, int c) {
     this->h = h;
     this->c = c;
     this->ar = (float)w / (float)h;
-    this->fb = new float[w * h * c]();
-    this->zb = new float[w * h];
+    this->fb = new float[c * h * w]();
+    this->zb = new float[h * w];
 
     for (int i = 0; i < w * h; i++) {this->zb[i] = 1.0f;}
 }
 
 void JTX::Core::Renderer::clear() {
-    for (int i = 0; i < w * h * c; i++) {
-        fb[i] = 0.0f;
-    }
-
-    for (int i = 0; i < w * h; i++) {
-        zb[i] = 1.0f;
-    }
+    // TODO: optimize this later
+    std::memset(fb, 0, c * h * w * sizeof(float));
+    std::memset(zb, 1, h * w * sizeof(float));
 }
 
 void JTX::Core::Renderer::render(JTX::Core::Scene *scene) {
-
+    // To-do
 }
+
+void JTX::Core::Renderer::drawLine(int x0, int y0, int x1, int y1, int ch, float val) {
+    if (x0 < 0 || x0 >= this->w || y0 < 0 || y0 >= this->h || x1 < 0 || x1 >= this->w || y1 < 0 || y1 >= this->h) {
+        throw std::invalid_argument("Line out of bounds");
+    }
+
+    if (x0 == x1) {
+        if (y0 > y1) {std::swap(y0, y1);}
+        for (int y = y0; y <= y1; ++y) {
+            drawPixel(x0, y, ch, val);
+        }
+        return;
+    }
+
+    if (y0 == y1) {
+        if (x0 > x1) {std::swap(x0, x1);}
+        for (int x = x0; x <= x1; ++x) {
+            drawPixel(x, y0, ch, val);
+        }
+        return;
+    }
+
+    bool steep = std::abs(y1 - y0) > std::abs(x1 - x0);
+    if (steep) {
+        std::swap(x0, y0);
+        std::swap(x1, y1);
+    }
+    if (x0 > x1) {
+        std::swap(x0, x1);
+        std::swap(y0, y1);
+    }
+
+    int dx = x1 - x0;
+    int dy = std::abs(y1 - y0);
+    int err = dx >> 1;
+    int sy = (y0 < y1) ? 1 : -1;
+    int y = y0;
+
+    if (steep) {
+        for (int x = x0; x <= x1; ++x) {
+            drawPixel(y, x, ch, val);
+            err -= dy;
+            if (err < 0) {
+                y += sy;
+                err += dx;
+            }
+        }
+    } else {
+        for (int x = x0; x <= x1; ++x) {
+            drawPixel(x, y, ch, val);
+            err -= dy;
+            if (err < 0) {
+                y += sy;
+                err += dx;
+            }
+        }
+    }
+}
+
--- JTX/Core/Renderer.h
+++ JTX/Core/Renderer.h
@@ -15,9 +15,33 @@ namespace JTX::Core {
         void clear();
         void render(Scene* scene);
 
-        [[nodiscard]] int getW() const { return w; }
-        [[nodiscard]] int getH() const { return h; }
-        [[nodiscard]] int getC() const { return c; }
+        inline void drawPixel(int x, int y, int channel, float val) {
+            if (x < 0 || x >= this->w || y < 0 || y >= this->h || channel < 0 || channel >= this->c) {
+                throw std::invalid_argument("Pixel out of bounds or invalid channel");
+            }
+            this->fb[channel * (this->h * this->w) + (y * this->w) + x] = val;
+        }
+        inline void drawPixel(int x, int y, float r, float g, float b) {
+            if (x < 0 || x >= this->w || y < 0 || y >= this->h || this->c != 3) {
+                throw std::invalid_argument("Pixel out of bounds or invalid channel");
+            }
+            this->fb[(y * this->w) + x] = r;
+            this->fb[(this->h * this->w) + (y * this->w) + x] = g;
+            this->fb[2 * (this->h * this->w) + (y * this->w) + x] = b;
+        }
+        inline float getPixel(int x, int y, int ch) {
+            if (x < 0 || x >= this->w || y < 0 || y >= this->h || ch < 0 || ch >= this->c) {
+                throw std::invalid_argument("Pixel out of bounds or invalid channel");
+            }
+            return this->fb[ch * (this->h * this->w) + (y * this->w) + x];
+        }
+
+        // Bresenhams Line
+        void drawLine(int x0, int y0, int x1, int y1, int ch, float val);
+
+        [[nodiscard]] int getWidth() const { return w; }
+        [[nodiscard]] int getHeight() const { return h; }
+        [[nodiscard]] int getChannels() const { return c; }
         [[nodiscard]] float getAR() const { return ar; }
         float *getFB() { return fb; }
         float *getZB() { return zb; }

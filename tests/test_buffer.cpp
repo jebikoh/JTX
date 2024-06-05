95815babf5dcdb726711713630e22d9d5c44d90a
Jayden E <aedara@uw.edu>
2024-06-05 01:13:47 -0700

    Add stress test flag; fix draw triangle test; remove RC head test

--- tests/test_buffer.cpp
+++ tests/test_buffer.cpp
@@ -10,6 +10,8 @@ static const std::string CUBE_PATH = "../../Tests/primitives/cube.obj";
 static const std::string HEAD_PATH = "../../Tests/primitives/head.obj";
 static const std::string GT3_PATH = "../../Tests/primitives/gt3rs.obj";
 
+static const bool ENABLE_STRESS_TESTS = false;
+
 TEST_CASE("Renderer saveFb() with crossing lines", "[SaveFB]") {
   JTX::Core::DefaultShader shader{};
   JTX::Core::Renderer r(1000, 1000, &shader, 3);
@@ -56,54 +58,9 @@ TEST_CASE("Head wireframe", "[SaveFB]") {
 TEST_CASE("Test draw triangle", "[SaveFB]") {
   JTX::Core::DefaultShader shader{};
   JTX::Core::Renderer r(1000, 1000, &shader, 3);
-  r.drawTriangle(300, 300, 0.0f, 700, 300, 0.0f, 500, 700, 0.0f,
+  r.drawTriangle(300, 300, 1.0f, 700, 300, 1.0f, 500, 700, 1.0f,
                  JTX::Util::WHITE);
   r.saveFb("triangle.png", 0);
-}
-
-TEST_CASE("Random color head", "[SaveFB]") {
-  JTX::Core::DefaultShader shader{};
-  JTX::Core::Renderer r(1000, 1000, &shader, 3);
-  JTX::Core::Primitive m{};
-  m.load(HEAD_PATH);
-
-  JTX::Util::Vec3f lookAt = {0.0f, 0.0f, -1.0f};
-
-  JTX::Util::Mat4 rot_z =
-      JTX::Util::Mat4::rotation(JTX::Util::degToRad(180), JTX::Util::Axis::Z);
-  JTX::Util::Mat4 scale2 = JTX::Util::Mat4::scale(499.0f, 499.0f, 1);
-  JTX::Util::Mat4 trans = JTX::Util::Mat4::translation(500.0f, 500.0f, 0);
-
-  m.applyTransform(&rot_z);
-  m.applyTransform(&scale2);
-  m.applyTransform(&trans);
-  m.calculateNormals();
-
-  for (int i = 0; i < m.getNumFaces(); i++) {
-    if (lookAt.dot(m.getNormal(i)[0], m.getNormal(i)[1], m.getNormal(i)[2]) >=
-        0) {
-      continue;
-    }
-
-    JTX::Core::Face *f = m.getFace(i);
-    float *v1 = m.getVertex(f->v1);
-    float *v2 = m.getVertex(f->v2);
-    float *v3 = m.getVertex(f->v3);
-
-    int v1_x = static_cast<int>(std::round(v1[0]));
-    int v1_y = static_cast<int>(std::round(v1[1]));
-    int v2_x = static_cast<int>(std::round(v2[0]));
-    int v2_y = static_cast<int>(std::round(v2[1]));
-    int v3_x = static_cast<int>(std::round(v3[0]));
-    int v3_y = static_cast<int>(std::round(v3[1]));
-
-    JTX::Util::Color color = {static_cast<float>(rand() % 255),
-                              static_cast<float>(rand() % 255),
-                              static_cast<float>(rand() % 255)};
-
-    r.drawTriangle(v1_x, v1_y, 0.0f, v2_x, v2_y, 0.0f, v3_x, v3_y, 0.0f, color);
-  }
-  r.saveFb("rc_head.png", 0);
 }
 
 TEST_CASE("Head perspective projection", "[SaveFB]") {
@@ -167,6 +124,9 @@ TEST_CASE("Cube perspective projection", "[SaveFB]") {
 }
 
 TEST_CASE("Perspective projection stress test", "[Stress]") {
+  if (ENABLE_STRESS_TESTS == false) {
+    return;
+  }
   JTX::Core::DefaultShader shader{};
   JTX::Core::Renderer r(1920, 1080, &shader, 3);
 

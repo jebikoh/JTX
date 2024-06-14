4256cf984bc65ac1deb42ce993518cee9506da6c
Jayden E <aedara@uw.edu>
2024-06-14 19:28:29 -0400

    Bring Vec4 to parity with Vec3; add vec4 tests

--- v2/src/util/vec4.hpp
+++ v2/src/util/vec4.hpp
@@ -1,37 +1,50 @@
 #pragma once
 
-#include "constants.hpp"
+#include "./assert.h"
+#include "./constants.hpp"
+#include "./numerical.h"
 #include <cmath>
 #include <stdexcept>
 
 namespace jtx {
-
     // Only allow numeric types
-    template <typename T, typename = std::enable_if_t<std::is_arithmetic_v<T>>>
+    template<typename T, typename = std::enable_if_t<std::is_arithmetic_v<T>>>
     class Vec4 {
     public:
         T x, y, z, w;
 
-        // Constructors
-        Vec4() : x(JTX_ZERO), y(JTX_ZERO), z(JTX_ZERO), w(JTX_ZERO) {};
-        Vec4(T x, T y, T z, T w) : x(x), y(y), z(z), w(w) {};
-        Vec4(const Vec4& other) : x(other.x), y(other.y), z(other.z), w(other.w) {};
-        ~Vec4() = default;
+        // Check for NaN
+        [[nodiscard]] inline bool valid() const {
+            return !(jtx::isNaN(x) || jtx::isNaN(y) || jtx::isNaN(z) || jtx::isNaN(w));
+        }
 
-        // Unary operators
+        //region Constructors
+        Vec4() : x(JTX_ZERO), y(JTX_ZERO), z(JTX_ZERO), w(JTX_ZERO){};
+
+        Vec4(T x, T y, T z, T w) : x(x), y(y), z(z), w(w) { ASSERT(valid()); };
+
+        Vec4(const Vec4 &other) : x(other.x), y(other.y), z(other.z), w(other.w) { ASSERT(valid()); };
+
+        ~Vec4() = default;
+        //endregion
+
+        //region Unary operators
         inline Vec4 operator-() const {
             return {-x, -y, -z, -w};
         }
+
         inline Vec4 operator+() const {
             return {+x, +y, +z, +w};
         }
-        inline Vec4& operator++() {
+
+        inline Vec4 &operator++() {
             x++;
             y++;
             z++;
             w++;
             return *this;
         }
+
         inline Vec4 operator++(int) {
             Vec4 temp = *this;
             x++;
@@ -40,13 +53,15 @@ namespace jtx {
             w++;
             return temp;
         }
-        inline Vec4& operator--() {
+
+        inline Vec4 &operator--() {
             x--;
             y--;
             z--;
             w--;
             return *this;
         }
+
         inline Vec4 operator--(int) {
             Vec4 temp = *this;
             x--;
@@ -55,85 +70,110 @@ namespace jtx {
             w--;
             return temp;
         }
+        //endregion
 
-        // Binary operators
-        inline Vec4& operator=(const Vec4& other) {
+        //region Binary operators
+        inline Vec4 &operator=(const Vec4 &other) {
+            ASSERT(other.valid());
             x = other.x;
             y = other.y;
             z = other.z;
             w = other.w;
             return *this;
         }
-        inline bool operator==(const Vec4& other) const {
+
+        inline bool operator==(const Vec4 &other) const {
             return (x == other.x) && (y == other.y) && (z == other.z) && (w == other.w);
         }
-        inline bool operator!=(const Vec4& other) const {
-            return (x != other.x) || (y != other.y) || (z != other.z) && (w != other.w);
+
+        inline bool operator!=(const Vec4 &other) const {
+            return (x != other.x) || (y != other.y) || (z != other.z) || (w != other.w);
         }
-        inline Vec4 operator+(const Vec4& other) const {
+
+        inline Vec4 operator+(const Vec4 &other) const {
             return {x + other.x, y + other.y, z + other.z, w + other.w};
         }
-        inline Vec4 operator-(const Vec4& other) const {
+
+        inline Vec4 operator-(const Vec4 &other) const {
             return {x - other.x, y - other.y, z - other.z, w - other.w};
         }
-        inline Vec4 operator*(const Vec4& other) const {
+
+        inline Vec4 operator*(const Vec4 &other) const {
             return {x * other.x, y * other.y, z * other.z, w * other.w};
         }
+
         inline Vec4 operator*(T scalar) const {
             return {x * scalar, y * scalar, z * scalar, w * scalar};
         }
-        inline Vec4 operator/(const Vec4& other) const{
+
+        inline Vec4 operator/(const Vec4 &other) const {
+            ASSERT(JTX_ZERO != other.x && JTX_ZERO != other.y && JTX_ZERO != other.z && JTX_ZERO != other.w);
             return {x / other.x, y / other.y, z / other.z, w / other.w};
         }
+
         inline Vec4 operator/(T scalar) const {
+            ASSERT(JTX_ZERO != scalar);
             return {x / scalar, y / scalar, z / scalar, w / scalar};
         }
+        //endregion
 
-        // In-place Assignment Operators
-        inline Vec4& operator+=(const Vec4& other) {
+        //region In-place Assignment Operators
+        inline Vec4 &operator+=(const Vec4 &other) {
             x += other.x;
             y += other.y;
             z += other.z;
             w += other.w;
+            ASSERT(valid());
             return *this;
         }
-        inline Vec4& operator-=(const Vec4& other) {
+
+        inline Vec4 &operator-=(const Vec4 &other) {
             x -= other.x;
             y -= other.y;
             z -= other.z;
             w -= other.w;
+            ASSERT(valid());
             return *this;
         }
-        inline Vec4& operator*=(const Vec4& other) {
+
+        inline Vec4 &operator*=(const Vec4 &other) {
             x *= other.x;
             y *= other.y;
             z *= other.z;
             w *= other.w;
+            ASSERT(valid());
             return *this;
         }
-        inline Vec4& operator*=(T scalar) {
+
+        inline Vec4 &operator*=(T scalar) {
             x *= scalar;
             y *= scalar;
             z *= scalar;
             w *= scalar;
+            ASSERT(valid());
             return *this;
         }
-        inline Vec4& operator/=(const Vec4& other) {
+
+        inline Vec4 &operator/=(const Vec4 &other) {
             x /= other.x;
             y /= other.y;
             z /= other.z;
             w /= other.w;
+            ASSERT(valid());
             return *this;
         }
-        inline Vec4& operator/=(T scalar) {
+
+        inline Vec4 &operator/=(T scalar) {
             x /= scalar;
             y /= scalar;
             z /= scalar;
             w /= scalar;
+            ASSERT(valid());
             return *this;
         }
+        //endregion
 
-        // Member functions
+        //region Member functions
         [[nodiscard]] inline T dot(const Vec4 &other) const {
             return x * other.x + y * other.y + z * other.z + w * other.w;
         }
@@ -144,7 +184,7 @@ namespace jtx {
 
         [[nodiscard]] inline float len() const { return std::sqrt(x * x + y * y + z * z + w * w); }
 
-        inline Vec4& normalize() {
+        inline Vec4 &normalize() {
             float l = len();
             if (l != 0) {
                 x /= l;
@@ -155,17 +195,86 @@ namespace jtx {
             return *this;
         }
 
-        [[nodiscard]] inline Vec4 normalized() const {
-            float l = len();
+        static inline Vec4 normalize(const Vec4 &v) {
+            float l = v.len();
             if (l != 0) {
-                return {x / l, y / l, z / l, w / l};
+                return {v.x / l, v.y / l, v.z / l, v.w / l};
             } else {
                 return Vec4{};
             }
         }
+
+        inline Vec4 &abs() {
+            x = std::abs(x);
+            y = std::abs(y);
+            z = std::abs(z);
+            w = std::abs(w);
+            return *this;
+        }
+
+        static inline Vec4 abs(const Vec4 &v) {
+            return {std::abs(v.x), std::abs(v.y), std::abs(v.z), std::abs(v.w)};
+        }
+
+        inline Vec4 &ceil() {
+            x = jtx::ceil(x);
+            y = jtx::ceil(y);
+            z = jtx::ceil(z);
+            w = jtx::ceil(w);
+            return *this;
+        }
+
+        static inline Vec4 ceil(const Vec4 &v) {
+            return {jtx::ceil(v.x), jtx::ceil(v.y), jtx::ceil(v.z), jtx::ceil(v.w)};
+        }
+
+        inline Vec4 &floor() {
+            x = jtx::floor(x);
+            y = jtx::floor(y);
+            z = jtx::floor(z);
+            w = jtx::floor(w);
+            return *this;
+        }
+
+        static inline Vec4 floor(const Vec4 &v) {
+            return {jtx::floor(v.x), jtx::floor(v.y), jtx::floor(v.z), jtx::floor(v.w)};
+        }
+
+        inline auto min() const {
+            return std::min(std::min(z, w), std::min(x, y));
+        }
+
+        static inline Vec4 min(const Vec4 &a, const Vec4 &b) {
+            return {std::min(a.x, b.x), std::min(a.y, b.y), std::min(a.z, b.z), std::min(a.w, b.w)};
+        }
+
+        inline auto max() const {
+            return std::max(std::max(z, w), std::max(x, y));
+        }
+
+        static inline Vec4 max(const Vec4 &a, const Vec4 &b) {
+            return {std::max(a.x, b.x), std::max(a.y, b.y), std::max(a.z, b.z), std::max(a.w, b.w)};
+        }
+
+        inline T hprod() const {
+            return x * y * z * w;
+        }
+
+        static inline T hprod(const Vec4 &v) {
+            return v.x * v.y * v.z * v.w;
+        }
+
+        static inline auto lerp(const Vec4 &a, const Vec4 &b, float t) {
+            return a * (1 - t) + b * t;
+        }
+
+        static inline Vec4 fma(const Vec4 &a, const Vec4 &b, const Vec4 &c) {
+            return a * b + c;
+        }
+        //endregion
     };
+
 
     typedef Vec4<int> Vec4i;
     typedef Vec4<float> Vec4f;
-
 }// namespace jtx
--- v2/tests/test_vector.cpp
+++ v2/tests/test_vector.cpp
@@ -4,8 +4,9 @@
  * Only float versions are tested, which a few individual tests for integer versions
  */
 
-#include "../src/util/vec3.hpp"
 #include "../src/util/vec2.hpp"
+#include "../src/util/vec3.hpp"
+#include "../src/util/vec4.hpp"
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
 
@@ -63,7 +64,6 @@ TEST_CASE("Vec3f operator- on negative values") {
     REQUIRE(v2.x == 1.0f);
     REQUIRE(v2.y == 2.0f);
     REQUIRE(v2.z == 3.0f);
-
 }
 
 TEST_CASE("Vec3f operator+") {
@@ -515,7 +515,6 @@ TEST_CASE("Vec2f operator- on negative values") {
     jtx::Vec2f v2 = -v1;
     REQUIRE(v2.x == 1.0f);
     REQUIRE(v2.y == 2.0f);
-
 }
 
 TEST_CASE("Vec2f operator+") {
@@ -866,6 +865,498 @@ TEST_CASE("Vec2i floor (static)") {
     auto v2 = jtx::Vec2i::floor(v);
     REQUIRE(v2.x == 1);
     REQUIRE(v2.y == 2);
+}
+
+//endregion
+
+//region Vec4: Constructors
+TEST_CASE("Vec4f default constructor", "[Vec4]") {
+    jtx::Vec4f v;
+    REQUIRE(v.x == 0.0f);
+    REQUIRE(v.y == 0.0f);
+    REQUIRE(v.z == 0.0f);
+    REQUIRE(v.w == 0.0f);
+}
+
+TEST_CASE("Vec4f constructor", "[Vec4]") {
+    jtx::Vec4f v(1.0f, 2.0f, 3.0f, 4.0f);
+    REQUIRE(v.x == 1.0f);
+    REQUIRE(v.y == 2.0f);
+    REQUIRE(v.z == 3.0f);
+    REQUIRE(v.w == 4.0f);
+}
+
+TEST_CASE("Vec4f copy constructor") {
+    jtx::Vec4f v1(1.0f, 2.0f, 3.0f, 4.0f);
+    jtx::Vec4f v2(v1);
+    REQUIRE(v2.x == 1.0f);
+    REQUIRE(v2.y == 2.0f);
+    REQUIRE(v2.z == 3.0f);
+    REQUIRE(v2.w == 4.0f);
+}
+//endregion
+
+//region Vec4: Numerical validity
+TEST_CASE("Vec4f valid", "[Vec4]") {
+    jtx::Vec4f v(1.0f, 2.0f, 3.0f, 4.0f);
+    REQUIRE(v.valid());
+}
+
+// Vec4::valid() resulting in invalid can only be tested in release mode
+#ifdef NDEBUG
+TEST_CASE("Vec4f invalid", "[Vec4]") {
+    jtx::Vec4f v(1.0f, 2.0f, 4.0f, std::numeric_limits<float>::quiet_NaN());
+    REQUIRE(!v.valid());
+}
+#endif
+//endregion
+
+//region Vec4: Unary operators
+TEST_CASE("Vec4f operator- on positive values") {
+    jtx::Vec4f v1(1.0f, 2.0f, 3.0f, 4.0f);
+    jtx::Vec4f v2 = -v1;
+    REQUIRE(v2.x == -1.0f);
+    REQUIRE(v2.y == -2.0f);
+    REQUIRE(v2.z == -3.0f);
+    REQUIRE(v2.w == -4.0f);
+}
+
+TEST_CASE("Vec4f operator- on negative values") {
+    jtx::Vec4f v1(-1.0f, -2.0f, -3.0f, -4.0f);
+    jtx::Vec4f v2 = -v1;
+    REQUIRE(v2.x == 1.0f);
+    REQUIRE(v2.y == 2.0f);
+    REQUIRE(v2.z == 3.0f);
+    REQUIRE(v2.w == 4.0f);
+}
+
+TEST_CASE("Vec4f operator+") {
+    jtx::Vec4f v1(1.0f, 2.0f, 3.0f, 4.0f);
+    jtx::Vec4f v2 = +v1;
+    REQUIRE(v2.x == 1.0f);
+    REQUIRE(v2.y == 2.0f);
+    REQUIRE(v2.z == 3.0f);
+    REQUIRE(v2.w == 4.0f);
+}
+
+TEST_CASE("Vec4f ++ postfix operator") {
+    jtx::Vec4f v(1.0f, 2.0f, 3.0f, 4.0f);
+    jtx::Vec4f v2 = v++;
+    REQUIRE(v.x == 2.0f);
+    REQUIRE(v.y == 3.0f);
+    REQUIRE(v.z == 4.0f);
+    REQUIRE(v.w == 5.0f);
+    REQUIRE(v2.x == 1.0f);
+    REQUIRE(v2.y == 2.0f);
+    REQUIRE(v2.z == 3.0f);
+    REQUIRE(v2.w == 4.0f);
+}
+
+TEST_CASE("Vec4f ++ prefix operator") {
+    jtx::Vec4f v(1.0f, 2.0f, 3.0f, 4.0f);
+    jtx::Vec4f v2 = ++v;
+    REQUIRE(v.x == 2.0f);
+    REQUIRE(v.y == 3.0f);
+    REQUIRE(v.z == 4.0f);
+    REQUIRE(v.w == 5.0f);
+    REQUIRE(v2.x == 2.0f);
+    REQUIRE(v2.y == 3.0f);
+    REQUIRE(v2.z == 4.0f);
+    REQUIRE(v2.w == 5.0f);
+}
+
+TEST_CASE("Vec4f -- postfix operator") {
+    jtx::Vec4f v(1.0f, 2.0f, 3.0f, 4.0f);
+    jtx::Vec4f v2 = v--;
+    REQUIRE(v.x == 0.0f);
+    REQUIRE(v.y == 1.0f);
+    REQUIRE(v.z == 2.0f);
+    REQUIRE(v.w == 3.0f);
+    REQUIRE(v2.x == 1.0f);
+    REQUIRE(v2.y == 2.0f);
+    REQUIRE(v2.z == 3.0f);
+    REQUIRE(v2.w == 4.0f);
+}
+
+TEST_CASE("Vec4f -- prefix operator") {
+    jtx::Vec4f v(1.0f, 2.0f, 3.0f, 4.0f);
+    jtx::Vec4f v2 = --v;
+    REQUIRE(v.x == 0.0f);
+    REQUIRE(v.y == 1.0f);
+    REQUIRE(v.z == 2.0f);
+    REQUIRE(v.w == 3.0f);
+    REQUIRE(v2.x == 0.0f);
+    REQUIRE(v2.y == 1.0f);
+    REQUIRE(v2.z == 2.0f);
+    REQUIRE(v2.w == 3.0f);
+}
+//endregion
+
+//region Vec4: Binary operators
+TEST_CASE("Vec4f = operator", "[Vec4]") {
+    jtx::Vec4f v1(1.0f, 2.0f, 3.0f, 4.0f);
+    jtx::Vec4f v2(4.0f, 5.0f, 6.0f, 4.0f);
+    v2 = v1;
+    REQUIRE(v2.x == 1.0f);
+    REQUIRE(v2.y == 2.0f);
+    REQUIRE(v2.z == 3.0f);
+    REQUIRE(v2.w == 4.0f);
+}
+
+TEST_CASE("Vec4f == operator (equal)", "[Vec4]") {
+    jtx::Vec4f v1(1.0f, 2.0f, 3.0f, 4.0f);
+    jtx::Vec4f v2(1.0f, 2.0f, 3.0f, 4.0f);
+    REQUIRE(v1 == v2);
+}
+
+TEST_CASE("Vec4f == operator (unequal)", "[Vec4]") {
+    jtx::Vec4f v1(1.0f, 2.0f, 3.0f, 4.0f);
+    jtx::Vec4f v2(1.0f, 2.0f, 4.0f, 4.0f);
+    REQUIRE(!(v1 == v2));
+}
+
+TEST_CASE("Vec4f != operator (equal)", "[Vec4]") {
+    jtx::Vec4f v1(1.0f, 2.0f, 3.0f, 4.0f);
+    jtx::Vec4f v2(1.0f, 2.0f, 3.0f, 4.0f);
+    REQUIRE(!(v1 != v2));
+}
+
+TEST_CASE("Vec4f != operator (unequal)", "[Vec4]") {
+    jtx::Vec4f v1(1.0f, 2.0f, 3.0f, 4.0f);
+    jtx::Vec4f v2(1.0f, 2.0f, 4.0f, 4.0f);
+    REQUIRE(v1 != v2);
+}
+
+TEST_CASE("Vec4f + operator", "[Vec4]") {
+    jtx::Vec4f v1(1.0f, 2.0f, 3.0f, 4.0f);
+    jtx::Vec4f v2(4.0f, 5.0f, 6.0f, 7.0f);
+    jtx::Vec4f v3 = v1 + v2;
+    REQUIRE(v3.x == 5.0f);
+    REQUIRE(v3.y == 7.0f);
+    REQUIRE(v3.z == 9.0f);
+    REQUIRE(v3.w == 11.0f);
+}
+
+TEST_CASE("Vec4f - operator", "[Vec4]") {
+    jtx::Vec4f v1(1.0f, 2.0f, 3.0f, 4.0f);
+    jtx::Vec4f v2(4.0f, 5.0f, 6.0f, 7.0f);
+    jtx::Vec4f v3 = v1 - v2;
+    REQUIRE(v3.x == -3.0f);
+    REQUIRE(v3.y == -3.0f);
+    REQUIRE(v3.z == -3.0f);
+    REQUIRE(v3.w == -3.0f);
+}
+
+TEST_CASE("Vec4f * operator", "[Vec4]") {
+    jtx::Vec4f v1(1.0f, 2.0f, 3.0f, 4.0f);
+    jtx::Vec4f v2(4.0f, 5.0f, 6.0f, 7.0f);
+    jtx::Vec4f v3 = v1 * v2;
+    REQUIRE(v3.x == 4.0f);
+    REQUIRE(v3.y == 10.0f);
+    REQUIRE(v3.z == 18.0f);
+    REQUIRE(v3.w == 28.0f);
+}
+
+TEST_CASE("Vec4f * operator (scalar)", "[Vec4]") {
+    jtx::Vec4f v1(1.0f, 2.0f, 3.0f, 4.0f);
+    float s = 2.0f;
+    jtx::Vec4f v2 = v1 * s;
+    REQUIRE(v2.x == 2.0f);
+    REQUIRE(v2.y == 4.0f);
+    REQUIRE(v2.z == 6.0f);
+    REQUIRE(v2.w == 8.0f);
+}
+
+TEST_CASE("Vec4f / operator", "[Vec4]") {
+    jtx::Vec4f v1(4.0f, 10.0f, 18.0f, 28.0f);
+    jtx::Vec4f v2(4.0f, 5.0f, 6.0f, 7.0f);
+    jtx::Vec4f v3 = v1 / v2;
+    REQUIRE(v3.x == 1.0f);
+    REQUIRE(v3.y == 2.0f);
+    REQUIRE(v3.z == 3.0f);
+    REQUIRE(v3.w == 4.0f);
+}
+
+TEST_CASE("Vec4f / operator (scalar)", "[Vec4]") {
+    jtx::Vec4f v1(2.0f, 4.0f, 6.0f, 8.0f);
+    float s = 2.0f;
+    jtx::Vec4f v2 = v1 / s;
+    REQUIRE(v2.x == 1.0f);
+    REQUIRE(v2.y == 2.0f);
+    REQUIRE(v2.z == 3.0f);
+    REQUIRE(v2.w == 4.0f);
+}
+//endregion
+
+//region Vec4: In-place operators
+TEST_CASE("Vec4f += operator", "[Vec4]") {
+    jtx::Vec4f v1(1.0f, 2.0f, 3.0f, 4.0f);
+    jtx::Vec4f v2(4.0f, 5.0f, 6.0f, 7.0f);
+    v1 += v2;
+    REQUIRE(v1.x == 5.0f);
+    REQUIRE(v1.y == 7.0f);
+    REQUIRE(v1.z == 9.0f);
+    REQUIRE(v1.w == 11.0f);
+}
+
+TEST_CASE("Vec4f -= operator", "[Vec4]") {
+    jtx::Vec4f v1(1.0f, 2.0f, 3.0f, 4.0f);
+    jtx::Vec4f v2(4.0f, 5.0f, 6.0f, 7.0f);
+    v1 -= v2;
+    REQUIRE(v1.x == -3.0f);
+    REQUIRE(v1.y == -3.0f);
+    REQUIRE(v1.z == -3.0f);
+    REQUIRE(v1.w == -3.0f);
+}
+
+TEST_CASE("Vec4f *= operator", "[Vec4]") {
+    jtx::Vec4f v1(1.0f, 2.0f, 3.0f, 4.0f);
+    jtx::Vec4f v2(4.0f, 5.0f, 6.0f, 7.0f);
+    v1 *= v2;
+    REQUIRE(v1.x == 4.0f);
+    REQUIRE(v1.y == 10.0f);
+    REQUIRE(v1.z == 18.0f);
+    REQUIRE(v1.w == 28.0f);
+}
+
+TEST_CASE("Vec4f *= operator (scalar)", "[Vec4]") {
+    jtx::Vec4f v1(1.0f, 2.0f, 3.0f, 4.0f);
+    float s = 2.0f;
+    v1 *= s;
+    REQUIRE(v1.x == 2.0f);
+    REQUIRE(v1.y == 4.0f);
+    REQUIRE(v1.z == 6.0f);
+    REQUIRE(v1.w == 8.0f);
+}
+
+TEST_CASE("Vec4f /= operator", "[Vec4]") {
+    jtx::Vec4f v1(4.0f, 10.0f, 18.0f, 28.0f);
+    jtx::Vec4f v2(4.0f, 5.0f, 6.0f, 7.0f);
+    v1 /= v2;
+    REQUIRE(v1.x == 1.0f);
+    REQUIRE(v1.y == 2.0f);
+    REQUIRE(v1.z == 3.0f);
+    REQUIRE(v1.w == 4.0f);
+}
+
+TEST_CASE("Vec4f /= operator (scalar)", "[Vec4]") {
+    jtx::Vec4f v1(2.0f, 4.0f, 6.0f, 8.0f);
+    float s = 2.0f;
+    v1 /= s;
+    REQUIRE(v1.x == 1.0f);
+    REQUIRE(v1.y == 2.0f);
+    REQUIRE(v1.z == 3.0f);
+    REQUIRE(v1.w == 4.0f);
+}
+//endregion
+
+//region Vec4: Member functions
+
+TEST_CASE("Vec4f dot", "[Vec4]") {
+    jtx::Vec4f v1(1.0f, 2.0f, 3.0f, 4.0f);
+    jtx::Vec4f v2(4.0f, 5.0f, 6.0f, 7.0f);
+    auto dot = v1.dot(v2);
+    REQUIRE(dot == 60.0f);
+}
+
+TEST_CASE("Vec4f dot (with individual components)", "[Vec4]") {
+    jtx::Vec4f v1(1.0f, 2.0f, 3.0f, 4.0f);
+    auto dot = v1.dot(4.0f, 5.0f, 6.0f, 7.0f);
+    REQUIRE(dot == 60.0f);
+}
+
+TEST_CASE("Vec4f len", "[Vec4]") {
+    jtx::Vec4f v(1.0f, 2.0f, 3.0f, 4.0f);
+    auto len = v.len();
+    REQUIRE_THAT(len, Catch::Matchers::WithinRel(5.477225f, 0.0001f));
+}
+
+TEST_CASE("Vec4f normalize", "[Vec4]") {
+    jtx::Vec4f v(1.0f, 2.0f, 3.0f, 4.0f);
+    v.normalize();
+    REQUIRE_THAT(v.x, Catch::Matchers::WithinRel(0.182574f, 0.0001f));
+    REQUIRE_THAT(v.y, Catch::Matchers::WithinRel(0.365148f, 0.0001f));
+    REQUIRE_THAT(v.z, Catch::Matchers::WithinRel(0.547722f, 0.0001f));
+    REQUIRE_THAT(v.w, Catch::Matchers::WithinRel(0.730296f, 0.0001f));
+}
+
+TEST_CASE("Vec4f normalize (static)", "[Vec4]") {
+    jtx::Vec4f v(1.0f, 2.0f, 3.0f, 4.0f);
+    auto v2 = jtx::Vec4f::normalize(v);
+    REQUIRE_THAT(v2.x, Catch::Matchers::WithinRel(0.182574f, 0.0001f));
+    REQUIRE_THAT(v2.y, Catch::Matchers::WithinRel(0.365148f, 0.0001f));
+    REQUIRE_THAT(v2.z, Catch::Matchers::WithinRel(0.547722f, 0.0001f));
+    REQUIRE_THAT(v2.w, Catch::Matchers::WithinRel(0.730296f, 0.0001f));
+}
+
+TEST_CASE("Vec4f abs", "[Vec4]") {
+    jtx::Vec4f v(-1.0f, -2.0f, -3.0f, -4.0f);
+    v.abs();
+    REQUIRE(v.x == 1.0f);
+    REQUIRE(v.y == 2.0f);
+    REQUIRE(v.z == 3.0f);
+    REQUIRE(v.w == 4.0f);
+}
+
+TEST_CASE("Vec4f abs (static)", "[Vec4]") {
+    jtx::Vec4f v(-1.0f, -2.0f, -3.0f, -4.0f);
+    auto v2 = jtx::Vec4f::abs(v);
+    REQUIRE(v2.x == 1.0f);
+    REQUIRE(v2.y == 2.0f);
+    REQUIRE(v2.z == 3.0f);
+    REQUIRE(v2.w == 4.0f);
+}
+
+TEST_CASE("Vec4f ceil", "[Vec4]") {
+    jtx::Vec4f v(1.1f, 2.2f, 3.3f, 4.4f);
+    v.ceil();
+    REQUIRE(v.x == 2.0f);
+    REQUIRE(v.y == 3.0f);
+    REQUIRE(v.z == 4.0f);
+    REQUIRE(v.w == 5.0f);
+}
+
+TEST_CASE("Vec4f ceil (static)", "[Vec4]") {
+    jtx::Vec4f v(1.1f, 2.2f, 3.3f, 4.4f);
+    auto v2 = jtx::Vec4f::ceil(v);
+    REQUIRE(v2.x == 2.0f);
+    REQUIRE(v2.y == 3.0f);
+    REQUIRE(v2.z == 4.0f);
+    REQUIRE(v2.w == 5.0f);
+}
+
+TEST_CASE("Vec4f floor", "[Vec4]") {
+    jtx::Vec4f v(1.1f, 2.2f, 3.3f, 4.4f);
+    v.floor();
+    REQUIRE(v.x == 1.0f);
+    REQUIRE(v.y == 2.0f);
+    REQUIRE(v.z == 3.0f);
+    REQUIRE(v.w == 4.0f);
+}
+
+TEST_CASE("Vec4f floor (static)", "[Vec4]") {
+    jtx::Vec4f v(1.1f, 2.2f, 3.3f, 4.4f);
+    auto v2 = jtx::Vec4f::floor(v);
+    REQUIRE(v2.x == 1.0f);
+    REQUIRE(v2.y == 2.0f);
+    REQUIRE(v2.z == 3.0f);
+    REQUIRE(v2.w == 4.0f);
+}
+
+TEST_CASE("Vec4f min", "[Vec4]") {
+    jtx::Vec4f v(1.0f, 2.0f, 3.0f, 4.0f);
+    auto min = v.min();
+    REQUIRE(min == 1.0f);
+}
+
+TEST_CASE("Vec4f min two vectors", "[Vec4]") {
+    jtx::Vec4f v1(1.0f, 2.0f, 3.0f, 4.0f);
+    jtx::Vec4f v2(4.0f, 1.0f, 2.0f, 5.0f);
+    auto v3 = jtx::Vec4f::min(v1, v2);
+    REQUIRE(v3.x == 1.0f);
+    REQUIRE(v3.y == 1.0f);
+    REQUIRE(v3.z == 2.0f);
+    REQUIRE(v3.w == 4.0f);
+}
+
+TEST_CASE("Vec4f max", "[Vec4]") {
+    jtx::Vec4f v(1.0f, 2.0f, 3.0f, 4.0f);
+    auto max = v.max();
+    REQUIRE(max == 4.0f);
+}
+
+TEST_CASE("Vec4f max two vectors", "[Vec4]") {
+    jtx::Vec4f v1(1.0f, 2.0f, 3.0f, 4.0f);
+    jtx::Vec4f v2(4.0f, 1.0f, 2.0f, 5.0f);
+    auto v3 = jtx::Vec4f::max(v1, v2);
+    REQUIRE(v3.x == 4.0f);
+    REQUIRE(v3.y == 2.0f);
+    REQUIRE(v3.z == 3.0f);
+    REQUIRE(v3.w == 5.0f);
+}
+
+TEST_CASE("Vec4f hprod", "[Vec4]") {
+    jtx::Vec4f v(1.0f, 2.0f, 3.0f, 4.0f);
+    auto hprod = v.hprod();
+    REQUIRE(hprod == 24.0f);
+}
+
+TEST_CASE("Vec4f hprod (static)", "[Vec4]") {
+    jtx::Vec4f v(1.0f, 2.0f, 3.0f, 4.0f);
+    auto hprod = jtx::Vec4f::hprod(v);
+    REQUIRE(hprod == 24.0f);
+}
+
+TEST_CASE("Vec4f lerp", "[Vec4]") {
+    jtx::Vec4f v1(1.0f, 2.0f, 3.0f, 4.0f);
+    jtx::Vec4f v2(4.0f, 5.0f, 6.0f, 7.0f);
+    auto v3 = jtx::Vec4f::lerp(v1, v2, 0.5f);
+    REQUIRE(v3.x == 2.5f);
+    REQUIRE(v3.y == 3.5f);
+    REQUIRE(v3.z == 4.5f);
+    REQUIRE(v3.w == 5.5f);
+}
+
+TEST_CASE("Vec4f FMA", "[Vec4]") {
+    jtx::Vec4f v1(1.0f, 2.0f, 3.0f, 4.0f);
+    jtx::Vec4f v2(4.0f, 5.0f, 6.0f, 7.0f);
+    jtx::Vec4f v3(7.0f, 8.0f, 9.0f, 10.0f);
+    auto v4 = jtx::Vec4f::fma(v1, v2, v3);
+    REQUIRE(v4.x == 11.0f);
+    REQUIRE(v4.y == 18.0f);
+    REQUIRE(v4.z == 27.0f);
+    REQUIRE(v4.w == 38.0f);
+}
+//endregion
+
+//region Vec4i Tests
+TEST_CASE("Vec4i default constructor", "[Vec4]") {
+    jtx::Vec4i v;
+    REQUIRE(v.x == 0);
+    REQUIRE(v.y == 0);
+    REQUIRE(v.z == 0);
+    REQUIRE(v.w == 0);
+}
+
+TEST_CASE("Vec4i valid", "[Vec4]") {
+    jtx::Vec4i v(1, 2, 3, 4);
+    REQUIRE(v.valid());
+}
+
+TEST_CASE("Vec4i ceil") {
+    jtx::Vec4i v(1, 2, 3, 4);
+    v.ceil();
+    REQUIRE(v.x == 1);
+    REQUIRE(v.y == 2);
+    REQUIRE(v.z == 3);
+    REQUIRE(v.w == 4);
+}
+
+TEST_CASE("Vec4i ceil (static)") {
+    jtx::Vec4i v(1, 2, 3, 4);
+    auto v2 = jtx::Vec4i::ceil(v);
+    REQUIRE(v2.x == 1);
+    REQUIRE(v2.y == 2);
+    REQUIRE(v2.z == 3);
+    REQUIRE(v2.w == 4);
+}
+
+TEST_CASE("Vec4i floor") {
+    jtx::Vec4i v(1, 2, 3, 4);
+    v.floor();
+    REQUIRE(v.x == 1);
+    REQUIRE(v.y == 2);
+    REQUIRE(v.z == 3);
+    REQUIRE(v.w == 4);
+}
+
+TEST_CASE("Vec4i floor (static)") {
+    jtx::Vec4i v(1, 2, 3, 4);
+    auto v2 = jtx::Vec4i::floor(v);
+    REQUIRE(v2.x == 1);
+    REQUIRE(v2.y == 2);
+    REQUIRE(v2.z == 3);
+    REQUIRE(v2.w == 4);
 }
 
 //endregion

cfce68b426c8d39d2137753b967f42b5885c0f19
Jayden E <aneeshedara@gmail.com>
2024-07-06 23:22:35 -0400

    Move inverse and determinant fns to source file

--- src/util/matrix.hpp
+++ src/util/matrix.hpp
@@ -198,7 +198,7 @@ namespace jtx {
             return true;
         }
 
-        [[nodiscard]] inline Vec4f mul(const Vec4f &vec) const {
+        [[nodiscard]] Vec4f mul(const Vec4f &vec) const {
             Vec4f res;
             for (int i = 0; i < 4; ++i) {
                 res[i] = jtx::innerProdf(m[i][0], vec[0], m[i][1], vec[1], m[i][2], vec[2], m[i][3], vec[3]);
@@ -206,7 +206,7 @@ namespace jtx {
             return res;
         }
 
-        [[nodiscard]] inline Mat4 mul(const Mat4 &mat) const {
+        [[nodiscard]] Mat4 mul(const Mat4 &mat) const {
             Mat4 res;
             for (int i = 0; i < 4; ++i) {
                 for (int j = 0; j < 4; ++j) {
@@ -221,29 +221,7 @@ namespace jtx {
             return this->mul(other);
         }
 
-        // PBRT
-        [[nodiscard]] inline float determinant() const {
-            float s0 = jtx::dop(m[0][0], m[1][1], m[1][0], m[0][1]);
-            float s1 = jtx::dop(m[0][0], m[1][2], m[1][0], m[0][2]);
-            float s2 = jtx::dop(m[0][0], m[1][3], m[1][0], m[0][3]);
-
-            float s3 = jtx::dop(m[0][1], m[1][2], m[1][1], m[0][2]);
-            float s4 = jtx::dop(m[0][1], m[1][3], m[1][1], m[0][3]);
-            float s5 = jtx::dop(m[0][2], m[1][3], m[1][2], m[0][3]);
-
-            float c0 = jtx::dop(m[2][0], m[3][1], m[3][0], m[2][1]);
-            float c1 = jtx::dop(m[2][0], m[3][2], m[3][0], m[2][2]);
-            float c2 = jtx::dop(m[2][0], m[3][3], m[3][0], m[2][3]);
-
-            float c3 = jtx::dop(m[2][1], m[3][2], m[3][1], m[2][2]);
-            float c4 = jtx::dop(m[2][1], m[3][3], m[3][1], m[2][3]);
-            float c5 = jtx::dop(m[2][2], m[3][3], m[3][2], m[2][3]);
-
-            return jtx::dop(s0, c5, s1, c4) + jtx::dop(s2, c3, -s3, c2) +
-                   jtx::dop(s5, c0, s4, c1);
-        }
-
-        [[nodiscard]] inline Mat4 transpose() const {
+        [[nodiscard]] Mat4 transpose() const {
             Mat4 res;
             for (int i = 0; i < 4; ++i) {
                 for (int j = 0; j < 4; ++j) {
@@ -254,60 +232,9 @@ namespace jtx {
         }
 
         // PBRT
-        [[nodiscard]] inline std::optional<Mat4> inverse() const {
-            float s0 = jtx::dop(m[0][0], m[1][1], m[1][0], m[0][1]);
-            float s1 = jtx::dop(m[0][0], m[1][2], m[1][0], m[0][2]);
-            float s2 = jtx::dop(m[0][0], m[1][3], m[1][0], m[0][3]);
+        [[nodiscard]] float determinant() const;
 
-            float s3 = jtx::dop(m[0][1], m[1][2], m[1][1], m[0][2]);
-            float s4 = jtx::dop(m[0][1], m[1][3], m[1][1], m[0][3]);
-            float s5 = jtx::dop(m[0][2], m[1][3], m[1][2], m[0][3]);
-
-            float c0 = jtx::dop(m[2][0], m[3][1], m[3][0], m[2][1]);
-            float c1 = jtx::dop(m[2][0], m[3][2], m[3][0], m[2][2]);
-            float c2 = jtx::dop(m[2][0], m[3][3], m[3][0], m[2][3]);
-
-            float c3 = jtx::dop(m[2][1], m[3][2], m[3][1], m[2][2]);
-            float c4 = jtx::dop(m[2][1], m[3][3], m[3][1], m[2][3]);
-            float c5 = jtx::dop(m[2][2], m[3][3], m[3][2], m[2][3]);
-
-            float det = jtx::dop(s0, c5, s1, c4) + jtx::dop(s2, c3, -s3, c2) +
-                        jtx::dop(s5, c0, s4, c1);
-            if (det == 0.0f) {
-                return {};
-            }
-
-            float invDet = 1.0f / det;
-
-            return {{invDet * jtx::innerProd(m[1][1], c5, m[1][3], c3, -m[1][2], c4),
-                     invDet * jtx::innerProd(-m[0][1], c5, m[0][2], c4, -m[0][3], c3),
-                     invDet * jtx::innerProd(m[3][1], s5, m[3][3], s3, -m[3][2], s4),
-                     invDet * jtx::innerProd(-m[2][1], s5, m[2][2], s4, -m[2][3], s3),
-                     invDet * jtx::innerProd(-m[1][0], c5, m[1][2], c2, -m[1][3], c1),
-                     invDet * jtx::innerProd(m[0][0], c5, m[0][3], c1, -m[0][2], c2),
-                     invDet * jtx::innerProd(-m[3][0], s5, m[3][2], s2, -m[3][3], s1),
-                     invDet * jtx::innerProd(m[2][0], s5, m[2][3], s1, -m[2][2], s2),
-                     invDet * jtx::innerProd(m[1][0], c4, m[1][3], c0, -m[1][1], c2),
-                     invDet * jtx::innerProd(-m[0][0], c4, m[0][1], c2, -m[0][3], c0),
-                     invDet * jtx::innerProd(m[3][0], s4, m[3][3], s0, -m[3][1], s2),
-                     invDet * jtx::innerProd(-m[2][0], s4, m[2][1], s2, -m[2][3], s0),
-                     invDet * jtx::innerProd(-m[1][0], c3, m[1][1], c1, -m[1][2], c0),
-                     invDet * jtx::innerProd(m[0][0], c3, m[0][2], c0, -m[0][1], c1),
-                     invDet * jtx::innerProd(-m[3][0], s3, m[3][1], s1, -m[3][2], s0),
-                     invDet * jtx::innerProd(m[2][0], s3, m[2][2], s0, -m[2][1], s1)}};
-        }
-
-        [[nodiscard]] std::string toString() const {
-            std::string res;
-            for (auto i: m) {
-                for (int j = 0; j < 4; ++j) {
-                    res += std::to_string(i[j]) + " ";
-                }
-                res += "\n";
-            }
-            return res;
-        }
-
+        [[nodiscard]] std::optional<Mat4> inverse() const;
         //endregion
     private:
         float m[4][4];
